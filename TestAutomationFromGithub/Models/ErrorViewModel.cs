97e3ae4146af07f6cb4b657353314f0725066810
yck82 <yck82@LAPTOP-1IL2LK57>
2022-10-14 18:25:33 +0200

    Add project files.

---
+++ TestAutomationFromGithub/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestAutomationFromGithub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
